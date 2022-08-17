dbd9bdd091b99642674cb86dc507dd9c52297c32
RaKesh Mandal <thetechie.rk@gmail.com>
2022-08-17 18:50:00 +0530

    Add Roboto font, refactor css, enable BS5

--- FinalHooksR18/src/components/layout/Header.js
+++ FinalHooksR18/src/components/layout/Header.js
@@ -1,16 +1,18 @@
 export default function Header() {
   return (
-    <div className="jumbotron jumbotronheight">
-      <div className="row">
-        <div className="col-12 col-sm-4 text-center">
-          <h6 className="text-uppercase">October 19-20&nbsp;&nbsp;2019</h6>
-          <h6 className="text-uppercase">San Jose, California</h6>
-        </div>
-        <div className="col-12 col-sm-8 text-lg-right">
-          <div>
-            <img src="/images/SVCClogo.png" />
+    <div className="hero px-4 py-5">
+      <div className="container">
+        <div className="row flex-lg-row-reverse align-items-center">
+          <div className="col-lg-6">
+            <div>
+              <img src="/images/SVCClogo.png" alt="SVCC Logo" />
+            </div>
+            <h2>Silicon Valley Code Camp 2019</h2>
           </div>
-          <h2>Silicon Valley Code Camp 2019</h2>
+          <div className="col-lg-6">
+            <h6 className="text-uppercase">October 19-20 2019</h6>
+            <h6 className="text-uppercase">San Jose, California</h6>
+          </div>
         </div>
       </div>
     </div>
