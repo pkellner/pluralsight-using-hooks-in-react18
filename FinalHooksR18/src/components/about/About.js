1ba1872656a16557b274fcd2473874a702f3dd0e
RaKesh Mandal <thetechie.rk@gmail.com>
2022-08-20 23:17:46 +0530

    Optimize About page

--- FinalHooksR18/src/components/about/About.js
+++ FinalHooksR18/src/components/about/About.js
@@ -1,22 +1,26 @@
 export default function About() {
   return (
     <div className="container">
-      <div id="content" className="content">
-        <br />
-        <div className="contactInfo">
-          <h1 className="hTitle">Organizers</h1>
-          <br />
-          <img
+      <div id="content" className="content-wrapper">
+        <div className="about">
+          <h1 className="hTitle">About</h1>
+          {/* <img
             src="/images/svcc-organizers.jpeg"
             alt="image4"
-          />
-          <p>
-            Silicon Valley Code Camp is put on by a dedicated group of
-            volunteers whose mission is to both provide the highest quality
-            content built around the topic of computer code, as well as create
-            an environment where shared knowledge is paramount. The volunteers
-            not only include the organizers, but all the speakers in Addition!!
-          </p>
+          /> */}
+          <div className="card border-0 p-3">
+            <div className="card-body">
+              <p className="card-text">
+                Silicon Valley Code Camp is put on by a dedicated group of
+                volunteers whose mission is to both provide the highest quality
+                content built around the topic of computer code, as well as
+                create an environment where shared knowledge is paramount.
+                <br /> <br />
+                The volunteers not only include the organizers, but all the
+                speakers in Addition!!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
