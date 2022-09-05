88f0c316ca362b0867ca49b42f3d7b49f65251c0
pkellner <peter@peterkellner.net>
2022-09-04 17:07:41 -0700

    wip

---
+++ m3/m3-clip-01-data-binding-with-built-in-usestate/pages/module-3/clip-01/demo1.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Demo1() {
+  const [text1, setText1] = useState("first");
+  const [text2, setText2] = useState("last");
+  return (
+    <div className="container">
+      <h3>Simple State Management</h3>
+      
+      <input onChange={(e) => setText1(e.target.value)} value={text1} /> {text1}
+      <hr/>
+      <input onChange={(e) => setText2(e.target.value)} value={text2} /> {text2}
+    </div>
+  );
+}
