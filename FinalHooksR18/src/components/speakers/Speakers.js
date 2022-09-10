9c6683e6bc5930d79d1659f37143dc8fbd35952d
pkellner <peter@peterkellner.net>
2022-09-10 11:10:42 -0700

    wip

--- FinalHooksR18/src/components/speakers/Speakers.js
+++ FinalHooksR18/src/components/speakers/Speakers.js
@@ -1,5 +1,8 @@
 import React, { useContext } from "react";
-import { SpeakersDataContext, SpeakersDataProvider } from "../../contexts/SpeakersDataContext";
+import {
+  SpeakersDataContext,
+  SpeakersDataProvider,
+} from "../../contexts/SpeakersDataContext";
 import SpeakerMenu from "./SpeakerMenu";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { SpeakerMenuProvider } from "../../contexts/SpeakerMenuContext";
@@ -26,12 +29,32 @@ const SpeakersX = () => {
   );
 };
 
+const SpeakersX1 = () => {
+  const { data: speakerList, loadingStatus } = useContext(SpeakersDataContext);
+  const { darkTheme } = useContext(ThemeContext);
+
+  if (loadingStatus === "hasErrored") return <div>Errored on load</div>;
+  if (loadingStatus === "loading") return <div>Loading...</div>;
+
+  return (
+    <div className={darkTheme ? "theme-dark" : "theme-light"}>
+      <SpeakerMenu />
+      <div className="container">
+        <div className="row g-4">
+          <SpeakerMenuProvider>
+            <SpeakersList speakerList={speakerList} />
+          </SpeakerMenuProvider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function Speakers() {
   return (
     <SpeakersDataProvider>
-      <SpeakersX />
+      {() => SpeakersX1()}
+      <SpeakersX1 />
     </SpeakersDataProvider>
   );
 }
-
-
