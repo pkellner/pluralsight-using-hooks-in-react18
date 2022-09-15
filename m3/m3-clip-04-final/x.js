4a11a69e181eab7396b745f69f0c5bf2cbe32701
pkellner <peter@peterkellner.net>
2022-09-15 08:21:03 -0700

    wip

---
+++ m3/m3-clip-04-final/x.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import DemoApp from "./demoApp";
+
+let localStateValue = undefined;
+
+export default function Demo() {
+  function useMyState(initial) {
+    if (localStateValue === undefined) {
+      localStateValue = initial;
+    }
+    const setValue = (val) => {
+      localStateValue = val;
+      reRenderMe();
+    };
+    const retVals = [localStateValue, setValue];
+    return retVals;
+  }
+  const [cnt, setCnt] = useState(0);
+  useEffect(() => {
+    console.log('rendering...');
+  }, [cnt]);
+  function reRenderMe() {
+    console.log("reRenderMe called...");
+    setCnt(cnt + 1);
+  }
+  return <DemoApp useState={useMyState} />
+}
