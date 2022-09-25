3404d694ce743e0626e12f065576805a7df76a89
pkellner <peter@peterkellner.net>
2022-09-25 06:54:35 -0700

    wip

---
+++ m5/m5-clip-040-final/src/components/speakers/DeleteSpeakerButton.js
@@ -0,0 +1,22 @@
+export default function DeleteSpeakerButton({ id }) {
+  const { deleteSpeaker } = {
+    deleteSpeaker: () => {},
+  };
+  return (
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+        const confirmed = confirm(
+          "Are you sure you want to delete this speaker?"
+        );
+        if (confirmed) {
+          deleteSpeaker(id);
+        }
+      }}
+      className="btn btn-danger btn-sm"
+    >
+      <i className="fa fa-trash"></i>
+      {" Delete "}
+    </button>
+  );
+}
