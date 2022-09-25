e0a3d76b3fce7270ce68c46a97518262c1e3a095
pkellner <peter@peterkellner.net>
2022-09-25 16:09:33 -0700

    wip

---
+++ m5/m5-clip-070-final/src/components/hooks/useSpeakersData.js
@@ -0,0 +1,35 @@
+import useGeneralizedCrudMethods from "./useGeneralizedCrudMethods";
+
+function useSpeakersData(url, errorNotificationFn) {
+  const {
+    data,
+    error,
+    loadingStatus,
+    createRecord,
+    updateRecord,
+    deleteRecord,
+  } = useGeneralizedCrudMethods(url, errorNotificationFn);
+
+  function createSpeaker(speakerRec, callbackDone) {
+    createRecord(speakerRec, callbackDone);
+  }
+
+  function updateSpeaker(speakerRec, callbackDone) {
+    updateRecord(speakerRec, callbackDone);
+  }
+
+  function deleteSpeaker(id, callbackDone) {
+    deleteRecord(id, callbackDone);
+  }
+
+  return {
+    data,
+    loadingStatus,
+    error,
+    createSpeaker,
+    updateSpeaker,
+    deleteSpeaker,
+  };
+}
+
+export default useSpeakersData;
