75bf2a4f0b52e52e35d9ad7be2e21417c074f848
pkellner <peter@peterkellner.net>
2022-10-24 19:02:55 -0700

    wiop

--- m4/m4-clip-100/pages/demo-context.js
+++ m4/m4-clip-100/pages/demo-context.js
@@ -1,13 +1,22 @@
-import { createContext, useState } from "react";
-import DemoUseContext from "../src/components/misc/demo-usecontext";
+import { createContext, useState }
+  from "react";
+import DemoUseContext from
+  "../src/components/misc/demo-usecontext";
 
-export const ThemeContext = createContext({});
+export const ThemeContext = createContext();
+
+function Ch2() {
+  return <DemoUseContext />
+}
 
 export default function DemoContext() {
   const [theme, setTheme] = useState("dark");
   return (
-    <ThemeContext.Provider value={{theme,  setTheme}}>
-       <DemoUseContext />
+    <ThemeContext.Provider
+      value={{ theme, setTheme }}>
+      <Ch2>
+        <DemoUseContext />
+      </Ch2>
     </ThemeContext.Provider>
   );
 }
