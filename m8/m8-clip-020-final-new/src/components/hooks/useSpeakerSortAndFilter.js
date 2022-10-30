77841e7c5d8cbd2f55317ffb8df15af7c771e93e
pkellner <peter@peterkellner.net>
2022-10-30 06:03:37 -0700

    wip

---
+++ m8/m8-clip-020-final-new/src/components/hooks/useSpeakerSortAndFilter.js
@@ -0,0 +1,31 @@
+export default function useSpeakerSortAndFilter(
+  speakerList,
+  speakingSaturday,
+  speakingSunday,
+  searchText
+) {
+  console.log("useSpeakerSortAndFilter called");
+  return speakerList
+    ? speakerList
+        .filter(
+          ({ sat, sun }) => (speakingSaturday && sat) || (speakingSunday && sun)
+        )
+        .filter(({ firstName, lastName }) => {
+          return (
+            searchText.length === 0 ||
+            (firstName?.toLowerCase() + lastName?.toLowerCase()).includes(
+              searchText.toLowerCase()
+            )
+          );
+        })
+        .sort(function (a, b) {
+          if (a.firstName < b.firstName) {
+            return -1;
+          }
+          if (a.firstName > b.firstName) {
+            return 1;
+          }
+          return 0;
+        })
+    : [];
+}
