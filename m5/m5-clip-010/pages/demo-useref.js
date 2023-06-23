77df603c90817dadd48ec05f0165c7ba2935d7b7
pkellner <peter@peterkellner.net>
2023-06-23 16:07:09 -0700

    Fixed a problem with onMouseOver not causing a re-render and added a cnt state to fix that.

--- m5/m5-clip-010/pages/demo-useref.js
+++ m5/m5-clip-010/pages/demo-useref.js
@@ -1,19 +1,18 @@
-import { useRef } from "react";
-
+import { useRef, useState } from "react";
 export default function Demo() {
+
   // case #1 reference DOM
   const imgRef = useRef();
   // case #2 reference value that does not cause re-render
   const mouseOverCnt = useRef(0);
-
+  const [cnt, setCnt] = useState(0);
   return (
     <div className="container">
-      <img
-        src="/images/Speaker-1124.jpg"
+      <img src="/images/Speaker-1124.jpg"
         ref={imgRef}
         style={{ filter: "grayscale(100%)" }}
         onMouseOver={() => {
-          imgRef.current.style.filter = "grayscale(0%)";
+          imgRef.current.style.filter = "grayscale(0%)";setCnt(cnt+1)
           mouseOverCnt.current++;
         }}
         onMouseOut={() => {
--- m5/m5-clip-020/pages/demo-useref.js
+++ m5/m5-clip-020/pages/demo-useref.js
@@ -1,24 +1,23 @@
-import { useRef } from "react";
-
+import { useRef, useState } from "react";
 export default function Demo() {
+
   // case #1 reference DOM
   const imgRef = useRef();
   // case #2 reference value that does not cause re-render
   const mouseOverCnt = useRef(0);
-
+  const [cnt, setCnt] = useState(0);
   return (
     <div className="container">
-      <img
-        src="/images/Speaker-1124.jpg"
-        ref={imgRef}
-        style={{ filter: "grayscale(100%)" }}
-        onMouseOver={() => {
-          imgRef.current.style.filter = "grayscale(0%)";
-          mouseOverCnt.current++;
-        }}
-        onMouseOut={() => {
-          imgRef.current.style.filter = "grayscale(100%)";
-        }}
+      <img src="/images/Speaker-1124.jpg"
+           ref={imgRef}
+           style={{ filter: "grayscale(100%)" }}
+           onMouseOver={() => {
+             imgRef.current.style.filter = "grayscale(0%)";setCnt(cnt+1)
+             mouseOverCnt.current++;
+           }}
+           onMouseOut={() => {
+             imgRef.current.style.filter = "grayscale(100%)";
+           }}
       />
       <hr />
       <button
